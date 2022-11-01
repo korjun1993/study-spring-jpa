903175bd6702cdd1a502138f2e0fbfc1cb6ce76d
korjun1993 <corojoon93@naver.com>
2022-11-01 17:32:56 +0900

    기능 실험 : postman 으로 Json data 전달받아 객체 생성하기
    입력받은 Json 형식의 문자열 데이터를 objectMapper 에 전해주면 객체를 생성할 수 있다. 스프링 부트로 Spring MVC 를 선택하면 기본적으로 Jackson 라이브러리 (ObjectMapper) 를 함께 제공한다.

---
+++ study-spring-mvc1/servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,24 @@
+package hello.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream(); // message body 내용을 byte 로 얻을 수 있다.
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8); // byte <-> 문자 변환시 인코딩 방법을 명시해야한다.
+        System.out.println("messageBody = " + messageBody);
+        response.getWriter().write("ok");
+    }
+}

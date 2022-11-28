815bbf44ef8ae7549b75fbcf16cfda050021a5be
korjun1993 <corojoon93@naver.com>
2022-11-28 17:06:04 +0900

    feat(MappingController) : PathVariable 및 추가 매핑 정보를 이용하는 컨트롤러 추가
    - PathVariable
    - 파라미터로 추가 매핑
    - 특정 헤더로 추가 매핑
    - Content-Type 헤더 기반 추가 매핑
    - Accept 헤더 기반 추가 매핑

---
+++ study-spring-mvc1-hello/src/main/java/studyspringmvc1/hello/basic/requestmapping/MappingController.java
@@ -0,0 +1,107 @@
+package studyspringmvc1.hello.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    /**
+     * 편리한 축약 애노테이션
+     *
+     */
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략 가능
+     *
+     * @PathVariable("userId") String userId -> @PathVariable userId
+     * /mapping/userA
+     */
+    @GetMapping("mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용 다중
+     * 변수명이 같으면 생략 가능
+     *
+     * @PathVariable("userId") String userId -> @PathVariable userId
+     * /mapping/userA
+     */
+    @GetMapping("mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    /**
+     * 파라미터로 추가 매핑 (파라미터도 매핑 조건으로 활용할 수 있음)
+     * params="mode"
+     * params="!mode"
+     * params="mode=debug"
+     * params="mode!=debug"
+     * params={"mode=debug","data=good"}
+     * localhost:8080/mapping-param?mode=debug
+     */
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    /**
+     * 특정 헤더로 추가 매핑
+     * headers="mode"
+     * headers=!mode" (header에 mode라는 key값이 없어야함)
+     * headers="mode=debug"
+     * headers="mode!=debug"
+     */
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     */
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    /**
+     * Accept 헤더 기반 Media Type
+     * produces = "text/html"
+     * produces = "!text/html"
+     * produces = "text/*"
+     * produces = "*\/*"
+     */
+    @PostMapping(value = "/mapping-produce", produces = "text/html")
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}

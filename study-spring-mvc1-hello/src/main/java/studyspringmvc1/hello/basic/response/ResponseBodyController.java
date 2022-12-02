7baed56ddaf02ff25f1f48db9315f3bdeb9204e4
korjun1993 <corojoon93@naver.com>
2022-12-02 14:36:21 +0900

    feat(ResponseBodyController) : Response body 에 데이터 넣는 예제
    - ResponseBody 를 사용하면 상태코드 입력이 어려운데, @ResponseStatus 애노테이션을 사용하면 된다. 하지만, 동적으로 상태코드를 변경해야된다면, 애노테이션을 사용하짖말고 ResponseEntity 를 이용하자.
    
    - @ResponseBody 를 클래스 레벨에 정의하면 모든 메서드가 HttpResponse Body 부분에 데이터를 쓰게 된다. 또한, ResponseBody + Controller 는 RestController 로 대체할 수 있다.

---
+++ study-spring-mvc1-hello/src/main/java/studyspringmvc1/hello/basic/response/ResponseBodyController.java
@@ -0,0 +1,53 @@
+package studyspringmvc1.hello.basic.response;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import studyspringmvc1.hello.basic.HelloData;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() throws IOException {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() {
+        return "ok";
+    }
+
+    /// JSON 처리 ///
+    @GetMapping("response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK) // HttpEntity 객체에 Http 상태코드를 싵는 것처럼 애노테이션만으로 상태코드를 지정할 수 있음.
+    @ResponseBody
+    @GetMapping("response-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return helloData;
+    }
+}

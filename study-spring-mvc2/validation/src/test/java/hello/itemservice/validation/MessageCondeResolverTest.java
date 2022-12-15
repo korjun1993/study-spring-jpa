3e27bd7cd9328254051cadb47f20a4949679a0bc
korjun1993 <corojoon93@naver.com>
2022-12-15 21:05:51 +0900

    test(MessageCodeResolverTest): BindingResult.reject(), rejectValue() 의 내부동작 알아보기
    - BindingResult.reject(), rejectValue() 는 내부에서 MessageCodesResolver 를 사용한다. 여기에서 메시지 코드들을 생성한다. FieldError, ObjectError 의 생성자를 보면, 오류 코드를 하나가 아니라 여러 오류 코드를 가질 수 있다. MessageCodesResolver 를 통해서 생성된 순서대로 오류 코드를 보관한다.
    - MessageCodesResolver : 인터페이스
    - DefaultMessageCodeResolver : 기본 구현체
    - 주로 다음과 함께 사용 ObjectError, FieldError
    - 객체 오류의 경우 다음 순서로 2가지 생성 : 1. code.objectName 2. code
    - 필드 오류의 경우 다음 순서로 4가지 생성 : 1.code.objectName.fieldName 2. code.fieldName 3. code.fieldType 4.code

---
+++ study-spring-mvc2/validation/src/test/java/hello/itemservice/validation/MessageCondeResolverTest.java
@@ -0,0 +1,36 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+public class MessageCondeResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        Assertions.assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+        Assertions.assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+//        rejectValue의 내부 동작
+//        bindingResult.rejectValue("itemName", "required");
+//        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+//        bindingResult.addError(new FieldError("item", "itemName", null, false, messageCodes, null, null));
+
+    }
+}

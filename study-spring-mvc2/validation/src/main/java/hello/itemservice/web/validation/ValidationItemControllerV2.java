01f84597eeaaa73244a6e40f959235f7d53757ee
korjun1993 <corojoon93@naver.com>
2022-12-15 23:37:16 +0900

    feat(ValidationItemControllerV2): Validated 애노테이션으로 검증 로직 자동으로 호출하기
    - ItemValidator 를 만들때, Validator 인터페이스를 구현했다. 이러면 스프링의 추가적인 도움을 받을 수 있다.
    - WebBinder 란 Binding 작업과 Validate 작업을 수행하는 역할을 한다.
    - WebBinder 는 컨트롤러에 요청이 올 때마다 만들어지는데, @InitBinder 애노테이션을 통해서 WebBinder 가 만들어질 때 WebBinder 에 대한 초기화 작업을 수행할 수 있다.
     - WebBinder.addValidator() 함수를 통해 우리가 만든 검증기 ItemValidator 를 추가할 수 있다.
     - ItemValidator 를 직접 호출하는 부분을 없애고, 검증 대상 앞에 @Validated 를 붙여주면 스프링이 대신 검증 로직을 호출한다. - 여러 검증기를 등록한다면 그 중에 어떤 검증기가 실행되어야할지 구분이 필요하다. 이 떄 Validator 의 supports() 가 사용된다.

--- study-spring-mvc2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ study-spring-mvc2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -23,6 +25,11 @@ public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
     private final ItemValidator itemValidator;
+
+    @InitBinder // Controller가 호출될때마다 Binder가 만들어지고, 그때마다 수행할 초기화 작업
+    public void init(WebDataBinder dataBinder) {
+        dataBinder.addValidators(itemValidator);
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -195,10 +202,37 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         //검증 로직
         itemValidator.validate(item, bindingResult);
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            //bindingResult 는 자동으로 Model 에 담긴다.
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        //검증대상 앞에 @Validated 를 붙인다: 검증 로직을 수행해줌
+//        itemValidator.validate(item, bindingResult);
+        /**
+         * @Validated 애노테이션이 붙으면 WebDataBinder에 등록한 검증기를 찾아서 실행한다.
+         * 그런데 여러 검증기를 등록한다면 그 중에 어떤 검증기가 실행되어야할지 구분이 필요하다.
+         * 이 때 Validator 의 supports() 가 사용된다.
+         * 여기서는 supports(Item.class) 호출되고, 결과가 true 이므로
+         * ItemValidator 의 validate() 가 호출된다.
+         */
 
         //검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {

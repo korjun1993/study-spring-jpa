60728db40ef3bc1e3e8d29d198143fbb117e91a7
korjun1993 <corojoon93@naver.com>
2022-12-19 17:08:47 +0900

    feat(login): Servlet이 제공하는 세션으로 로그인,로그아웃 처리
    - request.getSession(true) : 세션이 있으면 기존 세션을 반환하고, 세션이 없으면 새로운 세션을 생성해서 반환한다.
    - request.getSession(false) : 세션이 있으면 기존 세션을 반환하고, 세션이 없으면 새로운 세션을 생성하지 않고 null 을 반환한다.

--- study-spring-mvc2/login/src/main/java/hello/login/web/HomeController.java
+++ study-spring-mvc2/login/src/main/java/hello/login/web/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -41,7 +42,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String loginHomeV2(HttpServletRequest request, Model model) {
         Member loginMember = (Member) sessionManager.getSession(request);
 
@@ -49,6 +50,26 @@ public class HomeController {
             return "home";
         }
 
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String loginHomeV3(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인 처리된 화면으로 이동
         model.addAttribute("member", loginMember);
         return "loginHome";
     }

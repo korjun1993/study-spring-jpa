dfd1030ee601f4c9e156f6f6e7874b5b02107b63
korjun1993 <corojoon93@naver.com>
2022-12-26 16:46:14 +0900

    feat(BadRequestException): @ResponseStatus 애노테이션을 활용한 Exception 처리
    BadRequestException 예외가 컨트롤러 밖으로 넘어가면 ResponseStatusExceptionResolver 가 해당 애노테이션을 확인해서 오류 코드를 HttpStatus.BAD_REQUEST(400)으로 변경하고, 메시지도 담는다. ResponseStatusExceptionResolver 코드를 확인해보면 response.sendError(statusCode, resolvedReason) 을 호출하는 것을 확인할 수 있다. sendError(400) 을 호출했기 때문에 WAS에서 다시 오류 페이지(/error)를 내부 요청한다.

--- study-spring-mvc2/exception/src/main/java/hello/exception/api/APIExceptionController.java
+++ study-spring-mvc2/exception/src/main/java/hello/exception/api/APIExceptionController.java
@@ -1,12 +1,15 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -27,6 +30,11 @@ public class APIExceptionController {
         }
 
         return new MemberDto("id", "hello" + id);
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
     }
 
     @Data

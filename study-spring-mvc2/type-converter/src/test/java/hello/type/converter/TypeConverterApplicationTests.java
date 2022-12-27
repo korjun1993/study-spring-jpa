daca6a9dff6bf70bdea015a28cda91c0a4423143
korjun1993 <corojoon93@naver.com>
2022-12-27 20:58:19 +0900

    project setup

---
+++ study-spring-mvc2/type-converter/src/test/java/hello/type/converter/TypeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.type.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

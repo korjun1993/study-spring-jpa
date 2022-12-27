daf6e3979db886c41f4b146a9c78afe52590882c
korjun1993 <corojoon93@naver.com>
2022-12-27 21:10:01 +0900

    test(converter): IP,PORT <-> IpPort 컨버터 테스트 코드

---
+++ study-spring-mvc2/type-converter/src/test/java/hello/type/converter/converter/IpPortConverterTest.java
@@ -0,0 +1,24 @@
+package hello.type.converter.converter;
+
+import hello.type.converter.type.IpPort;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IpPortConverterTest {
+    @Test
+    void stringToIpPort() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortToString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+}

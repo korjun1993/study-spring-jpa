0c33017d4432faf5a0311b7d93c8a157c931cf7e
korjun1993 <corojoon93@naver.com>
2022-12-28 19:52:55 +0900

    config(WebConfig): Formatter 웹 애플리케이션에 적용

--- study-spring-mvc2/type-converter/src/main/java/hello/type/converter/WebConfig.java
+++ study-spring-mvc2/type-converter/src/main/java/hello/type/converter/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.type.converter;
 
 import hello.type.converter.converter.*;
+import hello.type.converter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,9 +11,17 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+/**
+ *         주석처리: 포맷터보다 컨버터가 우선순위가 높다.
+ *         아래 두개 컨버터는 MyNumberFormatter와 같은 대상을 변환하는 컨버터이다.
+ *         때문에 MyNumberFormatter를 테스트하기 위해 주석처리
+ */
+//        registry.addConverter(new StringToIntegerConverter()); //문자를 숫자로
+//        registry.addConverter(new IntegerToStringConverter()); //숫자를 문자로
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        //추가 문자<->숫자
+        registry.addFormatter(new MyNumberFormatter());
     }
 }

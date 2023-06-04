8fc4a13ca08f8cc48f2fa29aa735ea41eec18732
김호준 <corojoon93@naver.com>
2023-06-04 23:41:17 +0900

    feat: 언체크 예외는 적용 - 클라이언트코드는 언체크 예외 클래스에 대한 의존성이 생기지 않음

---
+++ study-spring-db1/jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -0,0 +1,66 @@
+package hello.jdbc.exception.basic;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UncheckedAppTest {
+    @Test
+    void checked() {
+        Controller controller = new Controller();
+        assertThatThrownBy(controller::request).isInstanceOf(RuntimeException.class);
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() {
+            service.logic();
+        }
+    }
+
+    static class Service {
+
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() {
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+    static class NetworkClient {
+        public void call() {
+            throw new RuntimeConnectionException("연결 실패");
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) {
+                throw new RuntimeSQLException(e);
+            }
+        }
+
+        public void runSQL() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException(Throwable cause) {
+            super(cause);
+        }
+    }
+
+    static class RuntimeConnectionException extends RuntimeException {
+        public RuntimeConnectionException(String message) {
+            super(message);
+        }
+    }
+}

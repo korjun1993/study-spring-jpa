fe75f0a4e5badee8a47f6d1a1b30c37eb9c8dd43
korjun1993 <corojoon93@naver.com>
2023-07-14 11:01:56 +0900

    Advisor 기반 프록시 적용

---
+++ study-spring-proxy/src/main/java/hello/proxy/config/v3_proxyfactory/advice/LogTraceAdvice.java
@@ -0,0 +1,33 @@
+package hello.proxy.config.v3_proxyfactory.advice;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class LogTraceAdvice implements MethodInterceptor {
+
+	private final LogTrace logTrace;
+
+	public LogTraceAdvice(LogTrace logTrace) {
+		this.logTrace = logTrace;
+	}
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		TraceStatus status = null;
+		try {
+			String message = invocation.getMethod().getName() + "," + invocation.getClass().getName() + "()";
+			status = logTrace.begin(message);
+
+			// 로직 호출
+			Object result = invocation.proceed();
+			logTrace.end(status);
+			return result;
+		} catch (Exception e) {
+			logTrace.exception(status, e);
+			throw e;
+		}
+	}
+}
